fb545b1dba28030b50d4910646493da10b4f2120
Love Holmström <36114731+logh17@users.noreply.github.com>
2023-10-10 17:04:51 +0200

    https://open.kattis.com/problems/cookingwater
    https://open.kattis.com/problems/cookingwater

---
+++ CookingWater/CookingWater.java
@@ -0,0 +1,26 @@
+// https://open.kattis.com/problems/cookingwater
+
+import java.util.Scanner;
+
+public class CookingWater {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] start = new int[n];
+        int[] end = new int [n];
+        for (int i = 0; i < n; i++) {
+            start[i] = scanner.nextInt();
+            end[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for ( int j = 0; j < n; j++) {
+                if (start[i] > end[j]) {
+                    System.out.println("edward is right");
+                    return;
+                }
+            }
+        }
+        System.out.println("gunilla has a point");
+    }
+}
